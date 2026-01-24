7d9f879bfd1e3fff9a65ec567cdfbfd1ea7462a3
richie20502 <richie.lugo.recillas.1990@gmail.com>
2026-01-24 11:24:57 -0600

    servicio

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hola mundodesde dev")
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def home():
+        return {'msj': 'hola mundoghghghghghg'}
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=  True, 
+            host= '0.0.0.0')
